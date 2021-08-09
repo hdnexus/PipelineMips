cb2ab77987915ade1a51720946a0caa26d749e46
matheus <werneckmatheus12@gmail.com>
2021-08-09 19:26:20 -0300

    Estrutura inicial do projeto

---
+++ compile.sh
@@ -0,0 +1,6 @@
+g++ -o main *.cpp
+if [ $? -eq 0 ];then 
+   echo "compilado com sucesso!"
+else
+   echo "nao compilado"
+fi
--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
+#include <fstream>
+
 using namespace std;
+
+
+void leArquivoTexto(string filename)
+{
+  ifstream arq(filename);
+  string linha;
+  if (arq.is_open())
+  {
+    //Vai ate o final do arquivo separando cada elemento do csv por ,
+    while (!arq.eof())
+    {
+      getline(arq, linha);
+      cout << linha << endl;
+    }
+  }
+  arq.close();
+}
 
 void menu()
 {
@@ -12,9 +31,27 @@ void menu()
   cout << "Professor: MARCELO LOBOSCO" << endl
        << endl;
 
-  cout << "Escolha uma opção para continuar:" << endl;
+  cout << "Seleciona o tipo do arquivo : " << endl;
+  cout << "1 -> Arquivo TXT" << endl;
+  cout << "2 -> Digitar pelo teclado os valores" << endl;
+
+  int option = 0;
+  cin >> option;
+  if (option == 1){
+    cout << "Digite o filename " << endl;
+    string filename;
+    cin >> filename;
+    leArquivoTexto(filename);
+  }
+
+  /*cout << "Escolha uma opção para continuar:" << endl;
   cout << "1 -> Execução passo a passo" << endl;
-  cout << "2 -> Execução direta" << endl;
+  cout << "2 -> Execução direta" << endl;*/
+}
+
+
+void lePeloTeclado()
+{
 }
 
 int main(int argc, char *argv[])
