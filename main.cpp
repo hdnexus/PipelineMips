010200f61f76fc739ef64f21447b1f4eda25309a
Gabriel <dotaes22@gmail.com>
2021-08-09 19:38:30 -0300

    Att

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-
 void leArquivoTexto(string filename)
 {
   ifstream arq(filename);
@@ -27,28 +26,52 @@ void menu()
   cout << "Trabalho de Organização de Computadores - Simulador de Pipeline" << endl
        << endl;
   cout << "Alunos: GABRIEL MARTINS QUINTANA VIEIRA MARQUES" << endl;
-  cout << "        MATHEUS GOMES LUZ WERNECK" << endl;
+  cout << "        MATHEUS GOMES LUZ WERNECK" << endl
+       << endl;
   cout << "Professor: MARCELO LOBOSCO" << endl
        << endl;
 
-  cout << "Seleciona o tipo do arquivo : " << endl;
+  cout << "Seleciona o tipo do arquivo: " << endl;
   cout << "1 -> Arquivo TXT" << endl;
   cout << "2 -> Digitar pelo teclado os valores" << endl;
 
   int option = 0;
   cin >> option;
-  if (option == 1){
-    cout << "Digite o filename " << endl;
+  if (option == 1)
+  {
+    cout << "Opção escolhida - leitura por arquivo txt" << endl;
+    cout << "Digite o filename: ";
     string filename;
     cin >> filename;
+    cout << "Saida: " << endl;
     leArquivoTexto(filename);
   }
 
-  /*cout << "Escolha uma opção para continuar:" << endl;
-  cout << "1 -> Execução passo a passo" << endl;
-  cout << "2 -> Execução direta" << endl;*/
-}
+  if (option == 2)
+  {
+    cout << "Opção escolhida - leitura pelo teclado" << endl;
+    cout << "Digite as instrucoes: ";
+    string instrucoes;
+    cin >> instrucoes;
+    cout << "Saida: " << endl;
+  }
 
+  cout << "Escolha uma opção para continuar:" << endl;
+  cout << "1 -> Execução passo a passo" << endl;
+  cout << "2 -> Execução direta" << endl;
+
+  int optionExec = 0;
+  cin >> optionExec;
+  if (optionExec == 1)
+  {
+    cout << "Opção escolhida - execução passo a passo" << endl;
+  }
+
+  if (option == 2)
+  {
+    cout << "Opção escolhida - execução direta" << endl;
+  }
+}
 
 void lePeloTeclado()
 {
