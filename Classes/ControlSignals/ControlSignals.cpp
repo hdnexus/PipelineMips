bb5d017cf6e269510b670261253f64575ccdc714
Gabriel <dotaes22@gmail.com>
2021-08-12 18:26:15 -0300

    Atualizacoes

--- Classes/ControlSignals/ControlSignals.cpp
+++ Classes/ControlSignals/ControlSignals.cpp
@@ -3,20 +3,20 @@
 ControlSignals::ControlSignals()
 {
   this->RegDst = 0;
-  this->Jump = 0;
-  this->Branch = 0;
-  this->MemRead = 0;
+  this->ALUSrc = 0;
   this->MemtoReg = 0;
+  this->RegWrite = 0;
+  this->MemRead = 0;
+  this->MemWrite = 0;
+  this->Branch = 0;
   this->ALUOp1 = 0;
   this->ALUOp0 = 0;
-  this->MemWrite = 0;
-  this->ALUSrc = 0;
-  this->RegWrite = 0;
+  this->Jump = 0;
 }
 
 void ControlSignals::setControlSignals(unsigned int opcode)
 {
-  if (opcode == 0) //R format: add, sub, and, or and slt
+  if (opcode == 0) //R format: add, sub, and, or, slt, sll, jr - OK
   {
     this->RegDst = 1;
     this->ALUSrc = 0;
@@ -30,49 +30,77 @@ void ControlSignals::setControlSignals(unsigned int opcode)
     this->Jump = 0;
   }
 
-  if (opcode == 4) ///beq
+  if (opcode == 2) //j
+  {
+    this->RegDst = 0;   // X - Don't care
+    this->ALUSrc = 0;   // X - Don't care
+    this->MemtoReg = 0; // X - Don't care
+    this->RegWrite = 0;
+    this->MemRead = 0; // X - Don't care
+    this->MemWrite = 0;
+    this->Branch = 0;
+    this->ALUOp1 = 0; // X - Don't care
+    this->ALUOp0 = 0; // X - Don't care
+    this->Jump = 1;
+  }
+
+  if (opcode == 3) //jal
+  {
+    this->RegDst = 0;   // X - Don't care
+    this->ALUSrc = 0;   // X - Don't care
+    this->MemtoReg = 0; // X - Don't care
+    this->RegWrite = 1;
+    this->MemRead = 0;
+    this->MemWrite = 0;
+    this->Branch = 0;
+    this->ALUOp1 = 0; // X - Don't care
+    this->ALUOp0 = 0; // X - Don't care
+    this->Jump = 1;
+  }
+
+  if (opcode == 4) //beq - OK
   {
     this->RegDst = 0; // X - Don't care
-    this->Jump = 0;
-    this->Branch = 1;
+    this->ALUSrc = 0;
+    this->MemtoReg = 0; // X - Don't care
+    this->RegWrite = 0;
     this->MemRead = 0;
-    this->MemtoReg = 0;
+    this->MemWrite = 0;
+    this->Branch = 1;
     this->ALUOp1 = 0;
     this->ALUOp0 = 1;
-    this->MemWrite = 0;
-    this->ALUSrc = 0;
-    this->RegWrite = 0;
+    this->Jump = 0;
   }
 
-  if (opcode == 5) ///Tipo I BNE
+  if (opcode == 5) //bne - Não tenho certeza
+  {
+    this->RegDst = 0; // X - Don't care
+    this->ALUSrc = 0;
+    this->MemtoReg = 0; // X - Don't care
+    this->RegWrite = 0;
+    this->MemRead = 0;
+    this->MemWrite = 0;
+    this->Branch = 1;
+    this->ALUOp1 = 0;
+    this->ALUOp0 = 1;
+    this->Jump = 0;
+  }
+
+  if (opcode == 8) //addi - OK
   {
     this->RegDst = 0;
-    this->Jump = 0;
-    this->Branch = 0;
-    this->MemRead = 0;
-    this->MemtoReg = 0;
-    this->ALUOp1 = 0;
-    this->ALUOp0 = 1;
-    this->MemWrite = 0;
     this->ALUSrc = 1;
-    this->RegWrite = 0;
-  }
-
-  if (opcode == 43) ///sw
-  {
-    this->RegDst = 0; //X - Don't care
-    this->Jump = 0;
-    this->Branch = 0;
-    this->MemRead = 0;
     this->MemtoReg = 0;
+    this->RegWrite = 1;
+    this->MemRead = 0;
+    this->MemWrite = 0;
+    this->Branch = 0;
     this->ALUOp1 = 0;
     this->ALUOp0 = 0;
-    this->MemWrite = 1;
-    this->ALUSrc = 1;
-    this->RegWrite = 0;
+    this->Jump = 0;
   }
 
-  if (opcode == 32) ///lw
+  if (opcode == 35) //lw - OK
   {
     this->RegDst = 0;
     this->ALUSrc = 1;
@@ -86,18 +114,17 @@ void ControlSignals::setControlSignals(unsigned int opcode)
     this->ALUOp0 = 0;
   }
 
-  if (opcode == 8) ///Tipo I ADDI
+  if (opcode == 43) //sw - OK
   {
-    this->RegDst = 0;
-    this->Jump = 0;
-    this->Branch = 0;
+    this->RegDst = 0; // X - Don't care
+    this->ALUSrc = 1;
+    this->MemtoReg = 0; // X - Don't care
+    this->RegWrite = 0;
     this->MemRead = 0;
-    this->MemtoReg = 0;
+    this->MemWrite = 1;
+    this->Branch = 0;
     this->ALUOp1 = 0;
     this->ALUOp0 = 0;
-    this->MemWrite = 0;
-    this->ALUSrc = 1;
-    this->RegWrite = 1;
+    this->Jump = 0;
   }
-  //Falta pra j, jr, jal e sll
 }
