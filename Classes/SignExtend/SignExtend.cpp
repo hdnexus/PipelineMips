d03a6a844248cfa361e5be2ab1283a876103d535
Gabriel Quintana <50339506+hdnexus@users.noreply.github.com>
2021-08-15 02:39:40 -0300

    Update SignExtend.cpp

--- Classes/SignExtend/SignExtend.cpp
+++ Classes/SignExtend/SignExtend.cpp
@@ -13,15 +13,19 @@ SignExtend::~SignExtend()
 bitset<32> SignExtend::ExtendingSign(int immediate)
 {
   string extendedImm;
+  string caseSignPositive = "0000000000000000"
+  string caseSignNegative = "1111111111111111"
   bitset<16> binary(immediate);
+  string addImmediate = binary.to_string();
   int sign = binary[15];
   if (sign == 0) // sinal positivo
   {
-    extendedImm = "0000000000000000" + binary.to_string<char, std::string::traits_type, std::string::allocator_type>(); // Ex: 0000 0000 0000 0000 0101 0101 0101 0101
+    extendedImm = caseSignPositive + addImmediate; // Ex: 0000 0000 0000 0000 0101 0101 0101 0101
   }
   else //sinal negativo
   {
-    extendedImm = "1111111111111111" + binary.to_string<char, std::string::traits_type, std::string::allocator_type>(); // Ex: 1111 1111 1111 1111 1101 0101 0101 0101
+    extendedImm = caseSignNegative + addImmediate; // Ex: 1111 1111 1111 1111 1101 0101 0101 0101
   }
+  
   return (bitset<32>(extendedImm));
 }
